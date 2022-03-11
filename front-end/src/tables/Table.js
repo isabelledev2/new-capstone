ea7978b328a4db52e009e7109de2b0025cf0f7f7
Isabelle Wallace <ibellwallace22@gmail.com>
2022-03-11 00:25:59 -0600

    pushing up frontend code fixed table.js

--- front-end/src/tables/Table.js
+++ front-end/src/tables/Table.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { finishTable } from "../utils/api";
@@ -42,49 +41,4 @@ export default function Table({ table, table_name, capacity, status }) {
             </div>
         </div>
     );
-=======
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import { finishTable } from "../utils/api";
-import ErrorAlert from "../layout/ErrorAlert";
-
-export default function Table({ table, table_name, capacity, status }) {
-
-    const [finishError, setFinishError] = useState([]);
-
-    const history = useHistory();
-
-    const finishHandler = (event) => {
-        event.preventDefault();
-        if (window.confirm("Is this table ready to seat new guests? This cannot be undone.")) {
-            const abortController = new AbortController();
-            // DELETE request
-            async function finish() {
-                try {
-                    await finishTable(table.table_id, abortController.signal);
-                    history.go(0);
-                } catch (error) {
-                    setFinishError([...finishError, error.message]);
-                }
-            }
-            if (finishError.length === 0) {
-                finish();
-            }
-        }
-    }
-
-    return (
-        <div className="card mb-3">
-            <ErrorAlert error={finishError} />
-            <h5 className="card-header">Table {table_name}</h5>
-            <div className="card-body">
-                <p>Capacity: {capacity}</p>
-                <p data-table-id-status={table.table_id}>Status: {status}</p>
-                {status === "Free" ? null : (
-                <button data-table-id-finish={table.table_id} className="btn btn-dark" type="button" onClick={finishHandler}>Finish</button>
-                )}
-            </div>
-        </div>
-    );
->>>>>>> c6842d8ce2a3a6fc2c01ee13f639b328a570eae5
 }
